f35a7f70dd79513342caa875537cab460d964c4d
Ola Haug Letrud <ola.letrud@gmail.com>
2026-08-25 18:42:12 +0200

    lagt til beregning av fahrenheit

--- session-01/01-lesson/04_average.py
+++ session-01/01-lesson/04_average.py
@@ -1,6 +1,6 @@
-temperature_morning = float(input("Registrér morgentemperatur: "))
-temperature_evening = float(input("Registrér kveldstemperatur: "))
-temperature_average = (temperature_evening + temperature_morning) / 2
+temperature_morning = float(input("Registrér morgentemperatur i Celsius: "))
+temperature_evening = float(input("Registrér kveldstemperatur i Celsius: "))
+temperature_average_celsius = (temperature_evening + temperature_morning) / 2
+temperature_average_fahrenheit = temperature_average_celsius * 9 / 5 + 32
 
-
-print(f"Gjennomsnittstemperaturen er {temperature_average} °C")
+print(f"Gjennomsnittstemperaturen er {temperature_average_celsius:.1f} °C eller {temperature_average_fahrenheit:.1f} °F")
