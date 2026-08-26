f369e173e978b47b62e8140d65f4f1d50c4c1e53
Ola Haug Letrud <ola.letrud@gmail.com>
2026-08-26 19:06:57 +0200

    new solution after misreading - tip and total

--- session-01/02-exercises/10_tip_and_total.py
+++ session-01/02-exercises/10_tip_and_total.py
@@ -1,6 +1,8 @@
-item = input("Skriv hvilken vare du skal selge: ")
-price = float(input("Skriv hva varen koster uten mva (kun tall): "))
-tax = price * 0.15
-total_price = price + tax
+item = input("Skriv hvilken vare du skal kjøpe: ")
+price = float(input("Skriv hva varen koster (kun tall): "))
+tip = price * 0.15
+total_price = price + tip
 
-print(f"Prisen for 1 stk {item} er kr {price:.2f},- pluss kr {tax:.2f},- i mva, altså totalt kr {total_price:.2f},-. ")
+print(f"Prisen for 1 stk {item} er kr {price:.2f},-")
+print(f"15% tips av kr {price:.2f} er kr {tip:.2f},-")
+print(f"Totalkostnad er {total_price:.2f},-. ")
