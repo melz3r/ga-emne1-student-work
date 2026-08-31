9b893f2c4025efbbcf6a5b33050dd202caf99fb6
Ola Haug Letrud <ola.letrud@gmail.com>
2026-08-31 18:26:34 +0200

    refactor: cost per person - bruker variabel

--- session-02/01-lesson/02_cost_per_person.py
+++ session-02/01-lesson/02_cost_per_person.py
@@ -1,6 +1,6 @@
 total_cost = float(input("Totalkostnad: "))
 people = int(input("Antall personer: "))
 
-cost_per_person = total_cost / 4
+cost_per_person = total_cost / people
 
 print(f"Kostnad pr person er kr {cost_per_person:.2f}")
