754a926d448015c7e1baf35eac584eca2795ab83
Ola Haug Letrud <ola.letrud@gmail.com>
2026-08-31 19:21:07 +0200

    for loops og slicing

---
+++ session-02/01-lesson/06_for_loops.py
@@ -0,0 +1,16 @@
+for loop_counter in range(1, 6):
+    print(f"Round {loop_counter}")
+print("Finished!")
+
+for loop_counter in range(0, 12, 3):
+    print(f"Round {loop_counter}")
+print("Finished!")
+
+for loop_counter in range(6, 0, -2):
+    print(f"Round {loop_counter}")
+print("Finished!")
+
+# Related topic: slicing
+name = "Ola Letrud"
+print(name[:3])
+print(name[4:10])
