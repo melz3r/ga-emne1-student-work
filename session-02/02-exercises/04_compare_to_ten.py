4deeacec77c8d4afa07bbbfeb27bf55a141048df
Ola Haug Letrud <ola.letrud@gmail.com>
2026-09-01 19:23:09 +0200

    simple else-if

---
+++ session-02/02-exercises/04_compare_to_ten.py
@@ -0,0 +1,8 @@
+integer = int(input("Heltall: "))
+
+if integer > 10:
+    print("The number is greater than 10.")
+elif integer < 10:
+    print("The number is less than 10.")
+else:
+    print("The number is equal to 10.")
