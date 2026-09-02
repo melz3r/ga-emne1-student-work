86400e681a8881783aebedc69452faf931370053
Ola Haug Letrud <ola.letrud@gmail.com>
2026-09-02 12:22:48 +0200

    count up using for loop and range()

---
+++ session-02/02-exercises/10_count_up.py
@@ -0,0 +1,11 @@
+for number in range(0, 10):
+    print(number)
+print ("Ferdig serie 1")
+
+for number in range(5, 16):
+    print(number)
+print ("Ferdig serie 2")
+
+for number in range(1, 21):
+    print(number)
+print("Ferdig serie 3")
