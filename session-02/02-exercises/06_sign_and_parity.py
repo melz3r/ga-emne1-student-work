7104311d75f0d23e9c7136ff1dcd8d5970d9bdd2
Ola Haug Letrud <ola.letrud@gmail.com>
2026-09-02 10:05:01 +0200

    refactor: check parity - extracted modulus to avoid duplicate

--- session-02/02-exercises/06_sign_and_parity.py
+++ session-02/02-exercises/06_sign_and_parity.py
@@ -4,18 +4,14 @@ modulus = integer % 2
 
 if integer > 0:
     print("Tallet er større enn null.")
-    if modulus == 0:
-        print("Tallet er et partall.")
-    elif modulus != 0:
-        print("Tallet er et oddetall.")
 elif integer < 0:
     print("Tallet er mindre enn null.")
-    if modulus == 0:
-        print("Tallet er et partall.")
-    elif modulus != 0:
-        print("Tallet er et oddetall.")
 else:
     print("Tallet er null.")
 
-
+if integer != 0:
+    if modulus == 0:
+        print("Tallet er et partall.")
+    else:
+        print("Tallet er et oddetall.")
 
