cd440a382dc3d01a1a8c3dad660e9db09a17ccd6
Ola Haug Letrud <ola.letrud@gmail.com>
2026-09-08 14:21:13 +0200

    Task 6.2 - PIN code - added is_authenticated check

--- 02-py-ifelse-loops/02-exercises/20_pin_check.py
+++ 02-py-ifelse-loops/02-exercises/20_pin_check.py
@@ -2,12 +2,14 @@ secret_pin = 2468
 attempts_left = 3
 is_authenticated = False
 
-while attempts_left > 0:
+while attempts_left > 0 and not is_authenticated:
     user_pin = int(input("Oppgi PIN: "))
     if user_pin == secret_pin:
         print("Welcome.")
+        is_authenticated = True
     else:
         attempts_left -= 1
         if attempts_left > 0:
             print(f"Feil pin, prøv igjen. Du har igjen {attempts_left} forsøk.")
-print("Antall forsøk oppbrukt.")
+if not is_authenticated:
+    print("Antall forsøk oppbrukt.")
